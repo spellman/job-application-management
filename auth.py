8428b3b75c8084100ae12d68a45b641fc5634757
Cort Spellman <spellman.cort@gmail.com>
2024-08-17 10:47:42 -0500

    Restart auth workflow when refresh token is expired
    Rather than crashing when the refresh token is expired, we delete the
    auth-tokens file and restart the auth workflow, as though we are
    authenticating for the first time. The result is pretty seamless :)

--- auth.py
+++ auth.py
@@ -1,11 +1,16 @@
+import os
 import os.path
 
+from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
+import web_pdb
 
-# If modifying these scopes, delete the file token.json.
+
+TOKEN_FILE = "token.json"
+# If modifying these scopes, delete the TOKEN_FILE.
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 
@@ -14,17 +19,22 @@ def auth():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(TOKEN_FILE):
+        creds = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            with web_pdb.catch_post_mortem():
+                try:
+                    creds.refresh(Request())
+                except RefreshError:
+                    os.unlink(TOKEN_FILE)
+                    return auth()
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open(TOKEN_FILE, "w") as token:
             token.write(creds.to_json())
 
     return creds
